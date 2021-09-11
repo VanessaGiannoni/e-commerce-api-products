bf82dd94714fd56ef5a1233e35dfcee428cc134e
VanessaGiannoni <vanessa.o.giannoni@gmail.com>
2021-09-10 21:02:28 -0300

    Excluida dependência repetida do springboot

---
+++ src/test/java/br/senac/dev/web/api/product/ECommerceApiProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.dev.web.api.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
