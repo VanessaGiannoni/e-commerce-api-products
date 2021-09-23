b3513c59cb75ca93552b4f13af60e892ea46c82c
VanessaGiannoni <vanessa.o.giannoni@gmail.com>
2021-09-23 20:28:01 -0300

    Criadas as rotas de alterar e deletar. Add tratativa de erros

--- src/main/java/br/senac/devweb/api/products/categoria/CategoriaService.java
+++ src/main/java/br/senac/devweb/api/products/categoria/CategoriaService.java
@@ -1,6 +1,8 @@
 package br.senac.devweb.api.products.categoria;
 
+import br.senac.devweb.api.products.exceptions.NotFoundException;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -27,10 +29,23 @@ public class CategoriaService {
     }
 
     public void deleteCategoria(Long id) {
-        Categoria categoria = this.categoriaRepository.findById(id).get();
-
+        Categoria categoria = this.getCategory(id);
         categoria.setStatus(Categoria.Status.INATIVO);
-
         this.categoriaRepository.save(categoria);
+    }
+
+    public Categoria getCategory(Long id) {
+        BooleanExpression filter =
+                QCategoria.categoria.id.eq(id)
+                        .and(QCategoria.categoria.status.eq(Categoria.Status.ATIVO));
+
+        return this.categoriaRepository.findOne(filter).orElseThrow(() -> new NotFoundException("Categoria não encontrada."));
+    }
+
+    public Categoria update(Long id, CategoriaRepresentation.CreateCategoria createOrUpdateCategoria) {
+        Categoria categoria = getCategory(id);
+        categoria.setDescricao(createOrUpdateCategoria.getDescricao());
+
+        return this.categoriaRepository.save(categoria);
     }
 }
