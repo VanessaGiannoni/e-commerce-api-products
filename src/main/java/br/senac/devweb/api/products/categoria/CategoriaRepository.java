f79a6c8335eced186b7b516e381b4518d9bede82
VanessaGiannoni <vanessa.o.giannoni@gmail.com>
2021-10-08 21:09:42 -0300

    Filtros dinâmicos adicionados na categoria

--- src/main/java/br/senac/devweb/api/products/categoria/CategoriaRepository.java
+++ src/main/java/br/senac/devweb/api/products/categoria/CategoriaRepository.java
@@ -1,13 +1,23 @@
 package br.senac.devweb.api.products.categoria;
 
 import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public interface CategoriaRepository extends PagingAndSortingRepository<Categoria, Long>, QuerydslPredicateExecutor<Categoria> {
+public interface CategoriaRepository extends PagingAndSortingRepository<Categoria, Long>,
+        QuerydslPredicateExecutor<Categoria>,
+        QuerydslBinderCustomizer<QCategoria> {
+
     List<Categoria> findAll(Predicate filter);
+
+    default void customize(QuerydslBindings bindings, QCategoria categoria) {
+        bindings.bind(categoria.descricao).first(StringExpression::containsIgnoreCase);
+    }
 }
